fd9941eb3f1944001dda7780f0fcddef735fca62
iMprov <improv00@mail.ru>
2025-07-21 12:47:48 +0700

    Сохраняет выбранную пиццу в localStorage

--- public/app.js
+++ public/app.js
@@ -12,6 +12,10 @@ function something()
 function add_to_cart(id) 
 {
 
+	var key = 'product_' + id;
 
-	alert('Вы выбрали пицу с id: ' + id);
+	var x = window.localStorage.getItem(key);
+	x = x * 1 + 1;
+	window.localStorage.setItem(key, x);
+
 }
